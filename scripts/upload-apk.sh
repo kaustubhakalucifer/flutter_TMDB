210667213980f95d61e5b609391686b1a6c372e9
Kaustubh Solanki <kaustubhsolanki98@gmail.com>
2022-01-14 00:10:53 +0530

    ls -l

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -5,6 +5,7 @@ git config --global user.email "no-reply@gactions.com"
 
 git clone --quiet --branch=apk https://kaustubhakalucifer:$PAT@github.com/kaustubhakalucifer/flutter_TMDB apk > /dev/null
 cd apk
+ls -l
 find ../build/app/outputs/apk/release/ -type f -name '*.apk' -exec cp -v {} . \;
 
 git checkout --orphan temporary
