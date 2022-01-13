04d6f342b85a68ba3045cbcfd3cc060105b75066
kaustubhsolanki <kaustubhsolanki98@gmail.com>
2022-01-13 21:39:48 +0530

    build test

---
+++ scripts/upload-apk.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git config --global user.name "Github Actions"
+git config --global user.email "no-reply@gactions.com"
+
+git clone --quit --branch=apk https://kaustubhakalucifer:$PAT@github.com//kaustubhakalucifer/flutter_TMDB apk > /dev/null
+
+cd apk
+
+find ../build -type f -name '*.apk' -execcp -v {} . \;
+
+git checkout --orphan temporary
+git add --all .
+git commit -am "[Auto] Update Apk ($(date +%Y-%m-%d.%H:%M:%S))"
+
+git branch -D apk
+git branch -m apk
+git push origin apk --force --quiet > /dev/null
