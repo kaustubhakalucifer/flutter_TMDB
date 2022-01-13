ed466f447772b9375f595b4a69d85d56d237e764
Kaustubh Solanki <kaustubhsolanki98@gmail.com>
2022-01-13 22:56:42 +0530

    final testing

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-ls -a
-# git config --global user.name "Kaustubh Solanki"
-# git config --global user.email "kaustubhsolanki98@gmail.com"
 
-# git clone https://kaustubhakalucifer:$PAT@github.com/kaustubhakalucifer/AndroidAppReleases
-# cd AndroidAppReleases
+git config --global user.name "Kaustubh Solanki"
+git config --global user.email "kaustubhsolanki98@gmail.com"
 
-# rm -rf ${GITHUB_REPOSITORY#*/}*
+git clone https://kaustubhakalucifer:$PAT@github.com/kaustubhakalucifer/AndroidAppReleases
+cd AndroidAppReleases
 
-# find app/build/outputs -type f -name '*.apk' -exec cp -v {} . \;
+rm -rf ${GITHUB_REPOSITORY#*/}*
 
-# git checkout --orphan temporary
+find build/app/outputs/apk/release/ -type f -name '*.apk' -exec cp -v {} . \;
 
-# for file in app*; do
-#     if [[ $file != *"unsigned"* ]];then
-#         mv $file ${GITHUB_REPOSITORY#*/}-${file#*-}
-#         git add ${GITHUB_REPOSITORY#*/}-${file#*-}
-#         git commit -am "Update ${GITHUB_REPOSITORY#*/}-${file#*-} ($(date +%Y-%m-%d.%H:%M:%S))"
-#     fi
-# done
+git checkout --orphan temporary
 
-# git branch -D main
-# git branch -m main
+for file in app*; do
+    if [[ $file == *"unsigned"* ]];then
+        mv $file ${GITHUB_REPOSITORY#*/}-${file#*-}
+        git add ${GITHUB_REPOSITORY#*/}-${file#*-}
+        git commit -am "Update ${GITHUB_REPOSITORY#*/}-${file#*-} ($(date +%Y-%m-%d.%H:%M:%S))"
+    fi
+done
 
-# git push origin main --force
+git branch -D main
+git branch -m main
+
+git push origin main --force
