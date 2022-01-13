6a9005348a2798d1dde561da878f3e1dbcdcab84
Kaustubh Solanki <kaustubhsolanki98@gmail.com>
2022-01-14 00:23:38 +0530

    release apk change

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -3,8 +3,8 @@
 git config --global user.name "Github Actions"
 git config --global user.email "no-reply@gactions.com"
 
-git clone --quiet --branch=apk https://kaustubhakalucifer:$PAT@github.com/kaustubhakalucifer/flutter_TMDB apk > /dev/null
-cd apk
+git clone --quiet --branch=release_apk https://kaustubhakalucifer:$PAT@github.com/kaustubhakalucifer/flutter_TMDB release_apk > /dev/null
+cd release_apk
 ls build/
 find ../build/app/outputs/apk/release/ -type f -name '*.apk' -exec cp -v {} . \;
 
@@ -16,4 +16,4 @@ git commit -am "[Auto] Update Apk ($(date +%Y-%m-%d.%H:%M:%S))"
 git branch -D apk
 git branch -m apk
 
-git push origin apk --force --quiet > /dev/null
+git push origin release_apk --force --quiet > /dev/null
